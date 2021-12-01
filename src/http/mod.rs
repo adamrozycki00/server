6cdec9f7314d1b6406e828bd66305a172f1a38d2
adam.rozycki <adam.rozycki@kyriba.com>
2021-12-01 19:02:54 +0100

    Implement Response

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,9 +1,13 @@
 mod method;
 mod query_string;
 mod request;
+mod response;
+mod status_code;
 
 pub use method::Method;
 pub use method::MethodError;
 pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
+pub use status_code::StatusCode;
