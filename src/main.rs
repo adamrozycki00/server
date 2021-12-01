8461742e0c563c848308cea2740fb8ba7971536a
adam.rozycki <adam.rozycki@kyriba.com>
2021-12-01 20:35:18 +0100

    Add public path

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ mod http;
 mod server;
 mod website_handler;
 
-use http::Method;
-use http::Request;
+use http::{Method, Request};
 use server::Server;
+use std::env;
+use std::fmt::format;
 use website_handler::WebsiteHandler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR")); // env! is used for reading the envs in compile time
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public_path={}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
