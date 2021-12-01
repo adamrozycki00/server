9911f20e5961872b16bc3bf014dd162eb78e62c6
adam.rozycki <adam.rozycki@kyriba.com>
2021-12-01 19:39:51 +0100

    Improve Response

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -26,11 +26,22 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>IT WORKS!</h1>".to_string()),
+                                    )
                                 }
-                                Err(e) => eprintln!("Failed to parse a request: {}", e),
+                                Err(e) => {
+                                    eprintln!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                eprintln!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => eprintln!("Failed to read from connection: {}", e),
