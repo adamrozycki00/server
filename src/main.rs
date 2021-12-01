4bf3c1695194b85075d4163639e6de1a0bd65cc7
adam.rozycki <adam.rozycki@kyriba.com>
2021-12-01 20:07:51 +0100

    Implement WebsiteHandler

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 
 mod http;
 mod server;
+mod website_handler;
 
 use http::Method;
 use http::Request;
 use server::Server;
+use website_handler::WebsiteHandler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,17 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::io::{Read, Write};
 use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 
 pub struct Server {
     address: String,
@@ -13,7 +22,7 @@ impl Server {
         Self { address }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.address);
 
         let listener = TcpListener::bind(&self.address).unwrap();
@@ -27,17 +36,8 @@ impl Server {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
                             let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => {
-                                    dbg!(request);
-                                    Response::new(
-                                        StatusCode::Ok,
-                                        Some("<h1>IT WORKS!</h1>".to_string()),
-                                    )
-                                }
-                                Err(e) => {
-                                    eprintln!("Failed to parse a request: {}", e);
-                                    Response::new(StatusCode::BadRequest, None)
-                                }
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
                             };
 
                             if let Err(e) = response.send(&mut stream) {
